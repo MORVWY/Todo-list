67c44bc8cc25efbdc1c8c442e1df51570020bc89
MORVWY <89845489+MORVWY@users.noreply.github.com>
2022-12-07 11:02:42 +0200

    Update tasks.js

--- js/tasks.js
+++ js/tasks.js
@@ -74,7 +74,7 @@ function displayTasks(item) {
         const checked = tasks.checked ? 'checked' : '';
 
         defaultTask += `
-            <div class="todo-list__item" id="${tasks.uniqueId}"><label><input type="checkbox" class="todo-list__item-checkbox" ${checked}><div><img src="../images/checked.svg"></div></label><input type="text" class="${completeTasks}" name="todo-list__item-name" value="${tasks.content}" readonly><div class="todo-list__item-buttons">
+            <div class="todo-list__item" id="${tasks.uniqueId}"><label><input type="checkbox" class="todo-list__item-checkbox" ${checked}><div><img src="images/checked.svg"></div></label><input type="text" class="${completeTasks}" name="todo-list__item-name" value="${tasks.content}" readonly><div class="todo-list__item-buttons">
                     <img src="images/edit-icon.svg" alt="edit icon" class="edit-todo">
                     <img src="images/delete-icon.svg" alt="delete icon" class="delete-todo">
                 </div>
